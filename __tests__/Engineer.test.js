7d43a156f4469d8fefc94e74746760b8b2bbf8f9
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-03-14 20:52:29 -0700

    All tests passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,7 +3,7 @@ const Engineer = require("../lib/Engineer");
 test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.github).toBe(testValue);
+  expect(e.getGithub()).toBe(testValue);
 });
 
 test("getRole() should return \"Engineer\"", () => {
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,5 +1,5 @@
 const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
+// const Employee = require("../lib/Employee");
 
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
