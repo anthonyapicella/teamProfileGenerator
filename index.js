76009b002dee4b25174f3b84e36b08410ebbbc7a
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-04-10 10:49:43 -0700

    finished employee questions

--- index.js
+++ index.js
@@ -31,50 +31,152 @@ function newMember() {
 					addManager();
 					break;
 				case 'Engineer':
-
+					addEngineer();
+					break;
 				case 'Intern':
+					addIntern();
+					break;
 			}
 		});
 }
 
 function addManager() {
-	inquirer.prompt([
-		{
-			name: 'name',
-			type: 'input',
-			message: 'What is your managers name?',
-		},
-		{
-			name: 'id',
-			type: 'input',
-			message: "Enter Manager's Employee ID number",
-		},
-		{
-			name: 'email',
-			type: 'input',
-			message: "Enter Manager's email address",
-		},
-		{
-			name: 'officeNumber',
-			type: 'input',
-			message: "Enter Manager's office number",
-		},
-        {
-			name: 'nextEmp',
-			type: 'confirm',
-			message: "Would you like to enter another employee?",
-		},
+	inquirer
+		.prompt([
+			{
+				name: 'name',
+				type: 'input',
+				message: 'What is your managers name?',
+			},
+			{
+				name: 'id',
+				type: 'input',
+				message: "Enter Manager's Employee ID number",
+			},
+			{
+				name: 'email',
+				type: 'input',
+				message: "Enter Manager's email address",
+			},
+			{
+				name: 'officeNumber',
+				type: 'input',
+				message: "Enter Manager's office number",
+			},
+			{
+				name: 'nextEmp',
+				type: 'confirm',
+				message: 'Would you like to add another employee?',
+			},
+		])
+		.then((res, err) => {
+			if (err) console.error(err);
+			const newManager = new Manager(
+				res.name,
+				res.id,
+				res.email,
+				res.officeNumber
+			);
+			employees.push(newManager);
+			console.log(employees);
 
-	])
-    .then((res, err) => {
-        if (err) console.error(err);
-        console.log(res.empType);
-        switch (res.nextEmp) {
-            case true:
-                newMember();
-                break;
-        }
-    });
+			if (res.nextEmp) {
+				newMember();
+			} else {
+				// return template function
+			}
+		});
+}
+
+function addEngineer() {
+	inquirer
+		.prompt([
+			{
+				name: 'name',
+				type: 'input',
+				message: "What is your Engineer's name?",
+			},
+			{
+				name: 'id',
+				type: 'input',
+				message: "Enter Engineer's Employee ID number",
+			},
+			{
+				name: 'email',
+				type: 'input',
+				message: "Enter Engineer's email address",
+			},
+			{
+				name: 'gitHub',
+				type: 'input',
+				message: "Enter Engineer's GitHub username",
+			},
+			{
+				name: 'nextEmp',
+				type: 'confirm',
+				message: 'Would you like to add another employee?',
+			},
+		])
+		.then((res, err) => {
+			if (err) console.error(err);
+			const newEngineer = new Engineer(
+				res.name,
+				res.id,
+				res.email,
+				res.gitHub
+			);
+			employees.push(newEngineer);
+			console.log(employees);
+
+			if (res.nextEmp) {
+				newMember();
+			} else {
+				// return template function
+			}
+		});
+}
+
+function addIntern() {
+	inquirer
+		.prompt([
+			{
+				name: 'name',
+				type: 'input',
+				message: "What is your Intern's name?",
+			},
+			{
+				name: 'id',
+				type: 'input',
+				message: "Enter Intern's Employee ID number",
+			},
+			{
+				name: 'email',
+				type: 'input',
+				message: "Enter Intern's email address",
+			},
+			{
+				name: 'school',
+				type: 'input',
+				message: "Enter Intern's School",
+			},
+			{
+				name: 'nextEmp',
+				type: 'confirm',
+				message: 'Would you like to add another employee?',
+			},
+		])
+		.then((res, err) => {
+			if (err) console.error(err);
+			const newIntern = new Intern(
+				res.name,
+				res.id,
+				res.email,
+				res.school
+			);
+			employees.push(newIntern);
+			console.log(employees);
+
+		});
 }
 
 buildTeam();
